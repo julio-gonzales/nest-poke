547ab4f9188273d2f13ecb0875232450002e698c
julio <juliogonzales1302@gmail.com>
2025-04-16 22:56:21 -0400

    antes de insertar por lotes

---
+++ src/seed/seed.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { SeedService } from './seed.service';
+
+
+@Controller('seed')
+export class SeedController {
+  constructor(private readonly seedService: SeedService) {}
+
+
+
+  @Get()
+  async excuteSeed() {
+
+    
+    return this.seedService.executeSeed();
+  }
+
+}
